010069ee54bf5f967e0d1f7b2f11fea2368d8628
tiff178 <tiff178@gmail.com>
2023-05-05 21:10:00 -0700

    Add method writeSchedule & readSchedule

--- src/main/java/PSS/ScheduleController.java
+++ src/main/java/PSS/ScheduleController.java
@@ -1,5 +1,14 @@
 package PSS;
 
+import java.io.IOException;
+
+
 public class ScheduleController {
-    
+    public static void writeSchedule(Schedule filename) throws IOException {
+
+    }
+
+    public static void readSchedule (Schedule filename) {
+
+    }
 }
