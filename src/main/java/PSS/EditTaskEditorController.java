33db50f9b1f20ad4d18b49df801f939e5bb61ba9
alison <tmrfandom@gmail.com>
2023-05-08 16:22:21 -0700

    added edit task interface and set up delete task interface

--- src/main/java/PSS/EditTaskEditorController.java
+++ src/main/java/PSS/EditTaskEditorController.java
@@ -1,4 +1,162 @@
 package PSS;
 
+import javafx.beans.binding.Bindings;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+import static PSS.ScheduleController.*;
+
 public class EditTaskEditorController {
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
+    private Tasks editedTask;
+    @FXML private Text editTaskTitle;
+    @FXML private TextField taskNameTextField;
+    @FXML private VBox recurringOptionsBox;
+    @FXML private RadioButton transientButton, recurringButton, antiButton;
+    @FXML private ChoiceBox<String> taskTypeChoiceBox;
+    @FXML private DatePicker startDatePicker;
+    @FXML private Spinner startTimeHourSpinner;
+    @FXML private ChoiceBox<String> startTimeMinuteChoiceBox;
+    @FXML private ChoiceBox<String> startTimeAMPMChoiceBox;
+    @FXML private Spinner durationHourSpinner;
+    @FXML private ChoiceBox<String> durationChoiceBox;
+    @FXML private DatePicker endDatePicker;
+    @FXML private ChoiceBox<String> frequencyChoiceBox;
+    @FXML private Button editButton;
+    @FXML private DialogPane createTaskDialog;
+    private String[] transientTaskTypes = {"Visit", "Shopping", "Appointment"};
+    private String[] recurringTaskTypes = {"Class", "Study", "Sleep", "Exercise", "Work", "Meal"};
+    private String[] antiTaskTypes = {"Cancellation"};
+    private String[] minutes = {"00", "15", "30", "45"};
+    private String[] AMPM = {"AM", "PM"};
+    private String[] minute = {"0", "15", "30", "45"};
+    private String[] frequency = {"Daily", "Weekly"};
+
+    public void setTask(Tasks task){
+        editedTask = task;
+        System.out.println("success");
+        setInputBoxes();
+    }
+
+    public void setInputBoxes(){
+        //TODO: set title to task name using getter
+        editTaskTitle.setText("Editing 'Task'");
+        if (editedTask instanceof RecurringTasks){
+            recurringOptionsBox.setVisible(true);
+            taskTypeChoiceBox.getItems().removeAll(taskTypeChoiceBox.getItems());
+            taskTypeChoiceBox.getItems().addAll(recurringTaskTypes);
+            taskTypeChoiceBox.setValue(recurringTaskTypes[0]);
+        } else if (editedTask instanceof TransientTasks){
+            recurringOptionsBox.setVisible(false);
+            taskTypeChoiceBox.getItems().removeAll(taskTypeChoiceBox.getItems());
+            taskTypeChoiceBox.getItems().addAll(transientTaskTypes);
+            taskTypeChoiceBox.setValue(transientTaskTypes[0]);
+        } else { // TODO:  if (editedTask instanceof AntiTasks){
+            recurringOptionsBox.setVisible(false);
+            taskTypeChoiceBox.getItems().removeAll(taskTypeChoiceBox.getItems());
+            taskTypeChoiceBox.getItems().addAll(antiTaskTypes);
+            taskTypeChoiceBox.setValue(antiTaskTypes[0]);
+        }
+        //TODO: use getters for task attributes instead of default values here
+        taskNameTextField.setText("Task");
+        editButton.disableProperty().bind(
+                Bindings.isEmpty(taskNameTextField.textProperty()));
+        startDatePicker.setValue(LocalDate.now());
+        SpinnerValueFactory<Integer> hourFactory = new SpinnerValueFactory.IntegerSpinnerValueFactory(1, 12);
+        SpinnerValueFactory<Integer> hour24Factory = new SpinnerValueFactory.IntegerSpinnerValueFactory(0, 23);
+        hourFactory.setValue(12);
+        hour24Factory.setValue(1);
+        startTimeHourSpinner.setValueFactory(hourFactory);
+        startTimeMinuteChoiceBox.getItems().addAll(minutes);
+        startTimeMinuteChoiceBox.setValue(minutes[0]);
+        startTimeAMPMChoiceBox.getItems().addAll(AMPM);
+        startTimeAMPMChoiceBox.setValue(AMPM[0]);
+        durationHourSpinner.setValueFactory(hour24Factory);
+        durationChoiceBox.getItems().addAll(minute);
+        durationChoiceBox.setValue(minute[0]);
+        endDatePicker.setValue(LocalDate.now());
+        frequencyChoiceBox.getItems().addAll(frequency);
+        frequencyChoiceBox.setValue(frequency[0]);
+    }
+
+    public void showDialogue(String title, String content){
+        Dialog<String> dialog = new Dialog<String>();
+        dialog.setTitle(title);
+        ButtonType type = new ButtonType("OK", ButtonBar.ButtonData.OK_DONE);
+        dialog.setContentText(content);
+        dialog.getDialogPane().getButtonTypes().add(type);
+        dialog.showAndWait();
+    }
+    public void editTask(ActionEvent event) throws IOException {
+        if (editedTask instanceof RecurringTasks) {
+            System.out.println("Task Class: Recurring");
+            //TODO: pass parameters, change to edit
+            if (createRecurringTask() == true){
+                showDialogue("Success", "Task successfully edited.");
+                switchToEditTask(event);
+            } else {
+                showDialogue("Error", "Task was not edited.");
+            }
+        } else if (editedTask instanceof TransientTasks) {
+            System.out.println("Task Class: Transient");
+            //TODO: pass parameters, change to edit
+            if (createTransientTask() == true){
+                showDialogue("Success", "Task successfully edited.");
+                switchToEditTask(event);
+            } else {
+                showDialogue("Error", "Task was not edited.");
+            }
+        } else { // TODO: if (editedTask instance of AntiTasks
+            System.out.println("Task Class: Anti");
+            //TODO: pass parameters, change to edit
+            if (createAntiTask() == true){
+                showDialogue("Success", "Task successfully edited.");
+                switchToEditTask(event);
+            } else {
+                showDialogue("Error", "Task was not edited.");
+            }
+        }
+        System.out.println("Name: " + taskNameTextField.getText());
+        System.out.println("Type: " + taskTypeChoiceBox.getValue());
+        LocalDate startDate = startDatePicker.getValue();
+        String formattedStartDate = startDate.format(DateTimeFormatter.BASIC_ISO_DATE);
+        System.out.println("Start Date: " + formattedStartDate);
+        System.out.println("Time: " + startTimeHourSpinner.getValue() + ":" + startTimeMinuteChoiceBox.getValue() + " " + startTimeAMPMChoiceBox.getValue());
+        System.out.println("Duration: " + durationHourSpinner.getValue() + ":" + durationChoiceBox.getValue());
+        if (editedTask instanceof RecurringTasks) {
+            LocalDate endDate = endDatePicker.getValue();
+            String formattedEndDate = endDate.format(DateTimeFormatter.BASIC_ISO_DATE);
+            System.out.println("End Date: " + formattedEndDate);
+            System.out.println("Frequency: " + frequencyChoiceBox.getValue());
+        }
+    }
+    public void switchToHome(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("home.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public void switchToEditTask(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("editTaskSearch.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
 }
--- src/main/java/PSS/ScheduleController.java
+++ src/main/java/PSS/ScheduleController.java
@@ -1,10 +1,6 @@
 package PSS;
 
-import java.io.*;
-import java.util.*;
-
-import org.json.simple.parser.*;
-import org.json.simple.JSONObject;
+import java.io.IOException;
 
 public class ScheduleController {
     //TODO: create parameters
@@ -17,42 +13,16 @@ public class ScheduleController {
     public static boolean createAntiTask(){
         return true;
     }
-
-    public static void writeSchedule(String filename){
-        JSONObject jsonObject = new JSONObject();
-        jsonObject.put("key", "value"); // need to insert the key and value names of tasks?
-        try {
-            FileWriter file = new FileWriter(filename);
-            file.write(jsonObject.toJSONString());
-            file.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        System.out.print(jsonObject);
+    public static Tasks findTask(String name){
+        Tasks exampleTask = new Tasks("task", 0f, 0f, 0);
+        return exampleTask;
     }
 
-    public static void readSchedule (String filename) {
-        File file = new File(filename); // check for file with specified name exist
-        if (file.exists()) {
-            System.out.println("File Exists");
-            JSONParser parser = new JSONParser();
-            try {
-                Object obj = parser.parse(new FileReader(filename));
-                JSONObject jsonObject = (JSONObject)obj;
-                Scanner scan = new Scanner(filename);
-                System.out.print((String)jsonObject.get(scan.nextLine())); // read each line one by one
-                // String name = (String)jsonObject.get("Name"); example code to get text from file
-            } catch (FileNotFoundException e) {
-                e.printStackTrace();
-            } catch (ParseException e) { // ParseException should throw an error if the file is not json type/format
-                e.printStackTrace();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            // check for errors in the file after reading (tasks cannot overlap, or have invalid/inconsistent details)
-        }
-        else {
-            System.out.println("File Does Not Exist");
-        }
+    public static void writeSchedule(Schedule filename) throws IOException {
+
+    }
+
+    public static void readSchedule (Schedule filename) {
+
     }
 }
